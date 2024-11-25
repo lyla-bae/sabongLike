f1d51953eee0f5ba82ef80e4fbeb729860b6a45b
sosojean <thwlsdl0989@gmail.com>
2024-11-25 22:25:42 +0900

    fix : Id 타입 변경

--- src/main/java/com/example/demo/volunteer/VolunteeringRepository.java
+++ src/main/java/com/example/demo/volunteer/VolunteeringRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VolunteeringRepository extends JpaRepository<Volunteering, Integer> {
+public interface VolunteeringRepository extends JpaRepository<Volunteering, Long> {
     @Override
     List<Volunteering> findAll();
 
