11ce3fe18aa6a3d8d3e09e02fa59f95561df3be4
sosojean <thwlsdl0989@gmail.com>
2024-11-30 21:31:45 +0900

    fix: 새로고침 에러 픽스

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -25,5 +26,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/templates/")
                 .addResourceLocations("classpath:/static/")
                 .setCacheControl(CacheControl.noCache());
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:[^\\.]*}")
+            .setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:[^\\.]*}")
+            .setViewName("forward:/index.html");
     }
 }
