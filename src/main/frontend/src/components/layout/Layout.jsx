7aac1fecf0cf7277c3b1a5a6a25292a940c77573
baejeong-eun <lovelyeun0914@gmail.com>
2024-11-30 14:20:50 +0900

    feat : 리워드페이지 추가

--- src/main/frontend/src/components/layout/Layout.jsx
+++ src/main/frontend/src/components/layout/Layout.jsx
@@ -4,7 +4,6 @@ import MainPage from "../../pages/MainPage";
 import {Route, Router, Routes} from "react-router-dom";
 import Detail from "../../pages/Detail";
 import {useState} from "react";
-import Certification from "../../pages/Certification";
 
 
 const Layout = () => {
@@ -17,7 +16,6 @@ const Layout = () => {
         <Routes>
           <Route path="/" element={<MainPage setPosition = {setPosition} />}/>
           <Route path="/detail/:id" element={<Detail setPosition = {setPosition} />}/>
-          <Route path="/certification" element= {<Certification/>}/>
 
         </Routes>
       </div>
---
+++ src/main/frontend/src/pages/RewardShop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RewardShop = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RewardShop;
